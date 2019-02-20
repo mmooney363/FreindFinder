668c8189818fc6e62931225df3cbd525432c03fd
Mike Mooney <mmooney363@gmail.com>
2019-02-19 23:03:52 -0500

    initial

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Harry',
+		photo: 'https://image.shutterstock.com/mosaic_250/2936380/640011838/stock-photo-handsome-unshaven-young-dark-skinned-male-laughing-out-loud-at-funny-meme-he-found-on-internet-640011838.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Brendan',
+		photo: 'https://image.shutterstock.com/mosaic_250/2936380/762790210/stock-photo-isolated-shot-of-young-handsome-male-with-beard-mustache-and-trendy-hairdo-wears-casual-grey-762790210.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Andrew',
+		photo: 'https://us.123rf.com/450wm/lopolo/lopolo1511/lopolo151101403/47498481-a-portrait-of-a-men-in-studio-gray-background.jpg?ver=6',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Timmy',
+		photo: 'http://bundlestorm.com/wp-content/uploads/2014/10/19-thumb.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Justin',
+		photo: 'https://st2.depositphotos.com/1715570/5595/i/950/depositphotos_55955771-stock-photo-modern-young-black-man-smiling.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Samantha',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrQg-NyXQcM933wq6hBhc1z-KBRR5ZFB6l8MdhBuhtEdEuteHw',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Kelly',
+		photo: 'https://cdn.acidcow.com/pics/20130515/women_eating_pasta_04.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Briana',
+		photo: 'https://www.stockvault.net/data/2011/04/12/121747/thumb16.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Tiffany',
+		photo: 'https://www.thelistinghouse.com/wp-content/uploads/2017/09/485734753.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Sara',
+		photo: 'https://us.123rf.com/450wm/rahultiwari3190/rahultiwari31901605/rahultiwari3190160500007/57909841-beautiful-indian-girl-in-traditional-indian-salwar-shuit-.jpg?ver=6',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
