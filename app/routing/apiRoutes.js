77df2777d6b5ca73c1b82365c63cba266a185565
Mike Mooney <mmooney363@gmail.com>
2019-02-21 23:12:55 -0500

    changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,12 +9,12 @@ module.exports = function(app) {
 
 
 	
-	app.get('../data/friends.js', function(req, res) {
+	app.get('/api/friends', function(req, res) {
 		res.json(friends);
 	});
 
 	
-	app.post('../data/friends.js', function(req, res) {
+	app.post('/api/friends/', function(req, res) {
 		
 		var userInput = req.body;
 		
